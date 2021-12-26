2f28c7f9123a477853c048cc9afb022907e2c75e
JairoDaniel17 <jairodaniel.mt@gmail.com>
2021-12-25 19:28:47 -0500

    listo

--- Algoritmos avanzados para ordenamiento/Heapsort.cpp
+++ Algoritmos avanzados para ordenamiento/Heapsort.cpp
@@ -10,71 +10,88 @@ o (n log n)
 Aclaración:
 •  No es estable.
 •  Método complejo.
-*/  
+*/
 #include <windows.h>
 #include <iostream>
 #define MAX 100
 using namespace std;
 
-class Heapsort {
-	public:
-		int lista[MAX];
-		int j, temp, hijo;
-		bool esmonticulo;
-		void heapsort(int[], int);
-		void monticulo(int[], int, int);
+class Heapsort
+{
+public:
+	int lista[MAX];
+	int j, temp, hijo;
+	bool esmonticulo;
+	void heapsort(int[], int);
+	void monticulo(int[], int, int);
 };
-void Heapsort::monticulo(int lista[], int primero, int ultimo) {
+void Heapsort::monticulo(int lista[], int primero, int ultimo)
+{
 	cout << endl
-	     << endl
-	     << "Nuevo Monticulo" << endl
-	     << endl;
+		 << endl
+		 << "Nuevo Monticulo" << endl
+		 << endl;
 	esmonticulo = false;
-	for (int k = primero; k <= ultimo; k++) {
+	for (int k = primero; k <= ultimo; k++)
+	{
 		cout << lista[k] << "\t";
 	}
 	cout << endl;
-	while ((primero <= ultimo / 2) && !esmonticulo) {
-		if (2 * primero == ultimo) {
+	while ((primero <= ultimo / 2) && !esmonticulo)
+	{
+		if (2 * primero == ultimo)
+		{
 			hijo = 2 * primero;
-		} else {
-			if (lista[2 * primero] > lista[2 * primero + 1]) {
+		}
+		else
+		{
+			if (lista[2 * primero] > lista[2 * primero + 1])
+			{
 				hijo = 2 * primero;
-			} else {
+			}
+			else
+			{
 				hijo = 2 * primero + 1;
 			}
 		}
-		if (lista[primero] < lista[hijo]) {
+		if (lista[primero] < lista[hijo])
+		{
 			cout << endl
-			     << endl
-			     << "Cambio de " << lista[primero] << " por " << lista[hijo] << endl
-			     << endl;
+				 << endl
+				 << "Cambio de " << lista[primero] << " por " << lista[hijo] << endl
+				 << endl;
 			temp = lista[primero];
 			lista[primero] = lista[hijo];
 			lista[hijo] = temp;
 			primero = hijo;
-		} else {
+		}
+		else
+		{
 			esmonticulo = true;
 		}
 	}
 }
 
-void Heapsort::heapsort(int lista[], int n) {
-	for (j = n / 2; j >= 0; j--) {
+void Heapsort::heapsort(int lista[], int n)
+{
+	for (j = n / 2; j >= 0; j--)
+	{
 		monticulo(lista, j, n);
 	}
-	for (j = n; j >= 1; j--) {
+	for (j = n; j >= 1; j--)
+	{
 		temp = lista[0];
 		lista[0] = lista[j];
 		lista[j] = temp;
 		cout << endl
-		     << "Sacamos al dato " << lista[j] << " de la lista." << endl
-		     << endl;
+			 << "Sacamos al dato " << lista[j] << " de la lista." << endl
+			 << endl;
 		cout << endl
-		     << "************************************************************" << endl
-		     << "Nueva Lista: " << endl
-		     << endl;
-		for (int k = 0; k < j; k++) {
+			 << "************************************************************" << endl
+			 << "Nueva Lista: " << endl
+			 << endl;
+		for (int k = 0; k < j; k++)
+		{
 			cout << lista[k] << "\t";
 		}
 		Sleep(2000);
@@ -83,31 +100,35 @@ void Heapsort::heapsort(int lista[], int n) {
 	}
 }
 
-main() {
+main()
+{
 	Heapsort h;
 	int c;
 	cout << "cuantos datos: ";
 	cin >> c;
-	for (int i = 1; i <= c; i++) {
+	for (int i = 1; i <= c; i++)
+	{
 		system("cls");
 		cout << "dato[" << i << "]: ";
 		cin >> h.lista[i];
 	}
 	system("cls");
 	/**/ cout << "Lista Original" << endl
-	          << endl;
-	for (int i = 1; i <= c; i++) {
+			  << endl;
+	for (int i = 1; i <= c; i++)
+	{
 		cout << h.lista[i] << "  ";
 	}
 	h.heapsort(h.lista, c);
 	cout << endl;
 	cout << endl
-	     << "Lista Ordenada" << endl
-	     << endl;
-	for (int i = 1; i <= c; i++) {
+		 << "Lista Ordenada" << endl
+		 << endl;
+	for (int i = 1; i <= c; i++)
+	{
 		cout << h.lista[i] << "  ";
 	}
 	cout << endl
-	     << endl;
+		 << endl;
 	system("pause");
 }
